45558a7d9c4ff8a3d46bb6d6e1dbf170d219b9b2
aitorzubiri <aitorgaillard99@gmail.com>
2025-10-01 10:03:15 +0200

    U2 2.ariketa bukatu behar da

---
+++ U2/ariketa1_u2/ariketa1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class ariketa2
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("U2-2.ariketa");
+    }
+}
+public class Eragiketak
+{
+    public static int batuketa(int a, int b)
+    {
+        return a + b;
+    }
+    public static int kenketa(int a, int b)
+    {
+        return a - b;
+    }
+}
---
+++ U2/ariketa1_u2/ariketa1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+class Ariketa1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ariketa1_U2___HASTEN:");
+        Thread aimarHaria = new Thread(Threads.aimar);
+        Thread nereaHaria = new Thread(Threads.nerea);
+        Thread jurgiHaria = new Thread(Threads.jurgi);
+
+        aimarHaria.Start();
+        nereaHaria.Start();
+        jurgiHaria.Start();
+        aimarHaria.Join();
+        nereaHaria.Join();
+        jurgiHaria.Join();
+
+        Console.WriteLine("Bukatuta");
+    }
+}
+static class Threads { 
+    public static void aimar()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Aimar" + i);
+            Thread.Sleep(300);
+        }
+    }
+    public static void nerea()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Nerea" + i);
+            Thread.Sleep(1000);
+        }
+    }
+    public static void jurgi()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Jurgi" + i);
+            Thread.Sleep(500);
+        }
+    }
+}
