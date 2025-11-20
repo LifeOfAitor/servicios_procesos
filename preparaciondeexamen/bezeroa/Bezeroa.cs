78e13c49ebed272719481821ec3a33126bd5477e
aitorzubiri <aitorgaillard99@gmail.com>
2025-11-20 13:31:01 +0100

    examenerako minimoa ikasi

---
+++ preparaciondeexamen/bezeroa/Bezeroa.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examen_prestatu_zerbitzaria
+{
+    public class Bezeroa
+    {
+        private const string ipadress = "127.0.0.1";
+        private const int portua = 5000;
+        private static NetworkStream ns = null;
+        private static StreamReader sr = null;
+        private static StreamWriter sw = null;
+        private static TcpClient client = null;
+
+        public static void Main()
+        {
+            zerbitzariraKonektatu();
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Zerbitzariari bidaltzeko: ");
+                    string bidaltzeko;
+                    bidaltzeko = Console.ReadLine();
+                    while (string.IsNullOrEmpty(bidaltzeko))
+                    {
+                        bidaltzeko = Console.ReadLine();
+                    }
+                    sw.WriteLine(bidaltzeko); // zerbitzariai bidali mezua
+
+                    //zerbitzaritik jasotzeko mezua
+                    Console.WriteLine();
+                    string jasotakoMezua = sr.ReadLine();
+                    Console.WriteLine($"Zerbitzariaren mezua: {jasotakoMezua}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static void zerbitzariraKonektatu()
+        {
+            //izena ezarri
+            Console.WriteLine("Sartu izena konektatzeko:");
+            string izena = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(izena))
+            {
+                izena = Console.ReadLine();
+            }
+            try
+            {
+                client = new TcpClient(ipadress, portua);
+                ns = client.GetStream();
+                sr = new StreamReader(ns, Encoding.UTF8);
+                sw = new StreamWriter(ns, Encoding.UTF8) { AutoFlush = true };
+
+                //bidali mezua zerbitzarira
+                sw.WriteLine(izena);
+
+                string mezua = sr.ReadLine();
+                Console.WriteLine(mezua);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                itxi();
+            }
+            
+        }
+
+        public static void itxi()
+        {
+            ns.Close();
+            sw.Close();
+            sr.Close();
+            client.Close();
+        }
+    }
+}
---
+++ preparaciondeexamen/examen_prestatu_zerbitzaria/Zerbitzaria.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examen_prestatu_zerbitzaria
+{
+    public class Zerbitzaria
+    {
+        private const string ipadress = "127.0.0.1";
+        private static object lockObj = new object();
+        private const int portua = 5000;
+        private static List<BezeroObjetua> bezeroak = new();
+        private static TcpListener server = null;
+
+        public static void Main()
+        {
+            hasiZerbitzaria();
+            while (true)
+            {
+                TcpClient c = server.AcceptTcpClient();
+                lock (lockObj)
+                {
+                    if (bezeroak.Count >= 5) // bezero limitea superatzean bota egiten du
+                    {
+                        c.Close();
+                        Console.WriteLine("Bezero limitea isitsita, ezin da gehiago onartu");
+                        continue;
+                    }
+                }
+                new Task( () => BezeroarekinGauzakEgin(c)).Start();
+            }
+
+
+        }
+
+        public static void hasiZerbitzaria()
+        {
+            server = new TcpListener(IPAddress.Parse(ipadress), portua);
+            server.Start();
+            Console.WriteLine("Zerbitzaria martxan dago");
+            Console.WriteLine("-----------------------");
+        }
+
+        public static void BezeroarekinGauzakEgin(TcpClient bezeroarenTCP)
+        {
+            using NetworkStream ns = bezeroarenTCP.GetStream();
+            using StreamWriter sw = new StreamWriter(ns) { AutoFlush = true };
+            using StreamReader sr = new StreamReader(ns);
+            // bezero berria (Objetua) sortu
+            string izena = sr.ReadLine();
+            BezeroObjetua bezeroa = new BezeroObjetua(izena, bezeroarenTCP);
+            lock (lockObj)
+            {
+                bezeroak.Add(bezeroa);
+            }
+            string mezua = $"{bezeroa.izena} konektatu da zerbitzarira";
+            sw.WriteLine(mezua);
+            Console.WriteLine(mezua);
+
+            try
+            {
+                while (true)
+                {
+                    //bezerotik jasotzeko mezua
+                    string jasotakoMezua = sr.ReadLine();
+                    Console.WriteLine($"{bezeroa.izena} mezua: {jasotakoMezua}");
+                    sw.WriteLine("Proba"); // bezeroari bidali mezua
+                }
+            }
+            catch (Exception ex)
+            {
+                ns.Close();
+                sw.Close();
+                sr.Close();
+                bezeroarenTCP.Close();
+                Console.WriteLine($"{bezeroa.izena} deskonektatu da");
+                lock (lockObj)
+                {
+                    bezeroak.Remove(bezeroa);
+                }
+            }
+        }
+    }
+}
